fea41a9f0bbf293fbb1bdfdd27a3f8081b7c5670
Miniuc <m.kuzemczak@gmail.com>
2018-08-11 19:24:04 +0200

    Podstawowe definicje klas pochodnych po Action. StraightLineMovAction: konstructor, lerp(), początek calculate(). TODO: kontynuować calculate lub być może czas na algortym jakobianowy w klasie Robot.

--- Action.cpp
+++ Action.cpp
@@ -1,7 +1,5 @@
 #include "Action.h"
 
-
-
 Action::Action()
 {
 }
@@ -9,4 +7,45 @@ Action::Action()
 
 Action::~Action()
 {
+}
+
+
+StraightLineMovAction::StraightLineMovAction(Eigen::Vector3d start, Eigen::Vector3d dest)
+{
+	setType(STRAIGHT_LINE);
+	
+	starting = start;
+	destination = dest;
+}
+
+void StraightLineMovAction::calculate()
+{
+	Lista<Eigen::Vector3d> path;
+	
+	lerp(path);
+	
+	
+}
+
+void StraightLineMovAction::lerp(Lista<Eigen::Vector3d> & path)
+{
+	Eigen::Vector3d v = destination - starting;
+	
+	double vmod = sqrt(pow(v[0], 2) + pow(v[1], 2) + pow(v[2], 2));
+	
+	double number = vmod / 10;
+	
+	int loops = (int)number;
+	
+	v.normalize();
+	
+	v *= 10;
+	
+	
+	for (int i = 1; i < loops; i++)
+	{
+		path.push_back(starting + i*v);
+	}
+	
+	path.push_back(destination);
 }
--- Action.h
+++ Action.h
@@ -1,30 +1,117 @@
 #pragma once
 
 #include "EigenAddons.h"
+#include "Lista.h"
+
 
 typedef enum 
 {
 	STRAIGHT_LINE,
 	FREE,
 	ARCH,
-	LOCAL_PART_CHANGE
-} PointType;
+	LOCAL_CHANGE,
+	DELAY
+} ActionType;
 
+
+/* Klasa Action, po której dziedzicz¹ klasy pochodne,
+ * pozwala na stworzenie jednolitej sekwencji, listy czynnoœci, które robot ma wykonaæ.
+ * 
+ * Ka¿da klasa pochodna dla Action przechowuje dane potrzebne
+ *  do wykonania danej czynnoœci (np. punkty poœrednie lub czas postoju), 
+ *  oraz metodê, która wykorzystuje te dane (np. interpoluje tor lub obs³uguje czasomierz). 
+ *
+ **/
 class Action
 {
 	// klasa bazowa, niewiele tu jest, wszystko w klasach pochodnych
 	
-	PointType type;
+	ActionType type;
 	
 public:
 	Action();
 	~Action();
+	
+	void setType(ActionType t)
+	{
+		type = t;
+	}
+	
+	virtual void calculate()
+	{
+	}
+	virtual void execute()
+	{
+	}
 };
 
 class StraightLineMovAction : public Action
 {
+	Eigen::Vector3d starting,
+					destination;
+	
+public:
+	StraightLineMovAction(Eigen::Vector3d start, Eigen::Vector3d dest);
+	~StraightLineMovAction()
+	{
+	}
+	
+	virtual void calculate();
+	virtual void execute();
+	
+	void lerp(Lista<Eigen::Vector3d> & path);
+};
+
+class FreeMovAction : public Action
+{
 	Eigen::Vector3d destination;
 	
 public:
+	FreeMovAction(Eigen::Vector3d dest);
+	~FreeMovAction();
+	
+	virtual void calculate()
+	{
+	}
+	virtual void execute()
+	{
+	}
+};
+
+class ArchMovAction : public Action
+{
+	Eigen::Vector3d starting, 
+					intermediate,
+					destination;
+	
+public:
+	ArchMovAction(Eigen::Vector3d start, Eigen::Vector3d inter,
+		Eigen::Vector3d dest);
+	
+	~ArchMovAction();
+	
+	virtual void calculate()
+	{
+	}
+	virtual void execute()
+	{
+	}
+	
+};
+
+class DelayAction : public Action
+{
+	double millis;
+	
+public:
+	DelayAction(double mill);
+	~DelayAction();
+	
+	virtual void calculate()
+	{
+	}
+	virtual void execute()
+	{
+	}
 };
 
